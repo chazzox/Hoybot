f666e22726230d1efbd254b69d7812fddde361b5
Charlie <42206116+chazzox@users.noreply.github.com>
2019-01-02 14:28:39 +0000

    Updated the bot a bit lol

--- Bot.py
+++ Bot.py
@@ -3,6 +3,11 @@ import asyncio
 
 client = discord.Client()
 
+bot_out = ""
+
+
+
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -12,17 +17,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    
     print(message.channel)
-    if message.content.startswith('hb!'):
-        counter = 0
-        tmp = await  message.channel.send('Hello')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+    
+    messag = message.content    
+    messag = str(messag).lower()
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    print(messag)
+
+    if messag.startswith('hb!'):
+        bot_out = str(input("Enter message to send"))
+        await  message.channel.send(bot_out, tts = True)
+
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
-        await message.channel.send(message.channel, 'Done sleeping')
+        await message.channel.send('Done sleeping')
+
+
+
 
 client.run('NTI1MDI5NzA0NjAwOTExODgy.Dvxo_w.SEa9sTIgvg-YIoZYue14ho_Bp-Y')
